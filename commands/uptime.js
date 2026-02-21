f47ceb2cccdea84ad8b3f10640d995038dfd1b6f
georges16388 <mentalalpha493@gmail.com>
2026-02-21 16:00:39 +0000

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -13,7 +13,7 @@ export async function uptime(client, message) {
 │ 💾 RAM: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(1)}MB
 │
 │ "Beyond limits, we rise."
-│     - GHG7 -
+│     - GHGX7 -
 └────────────────────┘`
     
     await client.sendMessage(remoteJid, { text: text })
