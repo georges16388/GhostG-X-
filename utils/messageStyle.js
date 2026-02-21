6d2409c9cc43f717b513458d6d4996050e7afe7d
georges16388 <mentalalpha493@gmail.com>
2026-02-21 22:55:44 +0000

    Update messageStyle.js

--- utils/messageStyle.js
+++ utils/messageStyle.js
@@ -1,18 +1,26 @@
-import fs from "fs"
-import stylizedChar from "./fancy.js"
+import fs from "fs";
+import stylizedChar from "./fancy.js";
+
+// Liste des images et MP3
+const images = ["GhostG-X.jpg", "GhostG-X7.jpg", "menu.jpg"];
+const audio = "GhostG-X.mp3";
 
 export default function stylizedCardMessage(text) {
+  // Choisir une image aléatoire
+  const randomImage = images[Math.floor(Math.random() * images.length)];
+
   return {
     text: stylizedChar(text),
     contextInfo: {
       externalAdReply: {
         title: "⏤͟͟͞ＧＨＯＳＴＧ－Ｘ",
         body: "⏤͟͟͞ＧＨＯＳＴＧ",
-        thumbnail: fs.readFileSync("./database/DigiX.jpg"),
-        sourceUrl: "https://whatsapp.com",
-        mediaType: 1,
-        renderLargerThumbnail: false
+        thumbnail: fs.readFileSync(`./database/${randomImage}`), // miniature aléatoire
+        mediaType: 1, // 1 = lien / media
+        renderLargerThumbnail: false,
+        sourceUrl: `database/${audio}`, // MP3 cliquable
+        mediaUrl: `database/${audio}`
       }
     }
-  }
+  };
 }
