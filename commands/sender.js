8197aec4f016b89d1fb84baa302961151bd96b9b
georges16388 <mentalalpha493@gmail.com>
2026-02-22 23:57:14 +0000

    Update sender.js

--- commands/sender.js
+++ commands/sender.js
@@ -1,21 +1,23 @@
 import stylizedChar from "../utils/fancy.js";
 async function sender(message, client, texts) {
 
-const remoteJid = message?.key?.remoteJid;  
+    const remoteJid = message?.key?.remoteJid;
 
-try {  
-    await client.sendMessage(remoteJid, {  
+    try {
+        await client.sendMessage(remoteJid, {
 
-        text: stylizedChar(`> _*${texts}*_\n\n⏤͟͟͞ＧＨＯＳＴＧ－Ｘ`),  
-  
-    });  
-} catch (e) {  
-    console.log(e)  
-    return;  
-}
+            text: stylizedChar(`> _*${texts}*_`),
+    
+        });
+    } catch (e) {
+        console.log(e)
+        return;
+    }
 
+   
 }
 
 //22677487520@s.whatsapp.net
 
 export default sender;
+
