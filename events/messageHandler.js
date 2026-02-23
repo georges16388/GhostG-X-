8b6629c912ca2f4faf048526d8f2054ecd4c2349
-ّ⸙𓆩ᴘʜᴀɴᴛᴏᴍ ፝֟ 𝐗 <mentalalpha493@gmail.com>
2026-02-23 03:12:17 +0000

    Update messageHandler.js

--- events/messageHandler.js
+++ events/messageHandler.js
@@ -1,3 +1,4 @@
+import connectToWhatsapp from './utils/connectToWhatsapp.js';
 import configmanager from "../utils/configmanager.js"
 import fs from 'fs/promises'
 import group from '../commands/group.js'
