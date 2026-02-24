f5937e29af8733a79738bbf93671bc51515c837a
-ّ⸙𓆩ᴘʜᴀɴᴛᴏᴍ ፝֟ 𝐗 <mentalalpha493@gmail.com>
2026-02-24 20:00:06 +0000

    Update react.js

--- utils/react.js
+++ utils/react.js
@@ -10,7 +10,7 @@ export default async function react(client, message){
 
         {
             react: {
-                text: '🎯',
+                text: '👍🏾',
 
                 key: message.key
             }
