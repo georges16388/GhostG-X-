9a3b0c67bac50eaafd1793e70371af946f3d9892
-ّ⸙𓆩ᴘʜᴀɴᴛᴏᴍ ፝֟ 𝐗 <mentalalpha493@gmail.com>
2026-02-24 21:49:51 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,6 @@ import fs from "fs";
 import path from "path";
 import send from "../utils/sendMessage.js";
 import configmanager from "../utils/configmanager.js";
-import fs from "fs";
 
 // 🔥 Lecture manuelle du .env
 let BOT_NUMBER = '226XXXX'; // fallback si non défini
