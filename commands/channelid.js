b46a7540cb5aa70678fa14987e15e5f06d579e6d
-ّ⸙𓆩ᴘʜᴀɴᴛᴏᴍ ፝֟ 𝐗 <mentalalpha493@gmail.com>
2026-02-25 13:24:31 +0000

    Update channelid.js

--- commands/channelid.js
+++ commands/channelid.js
@@ -1,34 +1,31 @@
 import send from "../utils/sendMessage.js";
 
 export default async function channelid(client, message) {
+    let jid = message.key.remoteJid;
+    if (!jid) return;
+
+    let type;
+    let chatName = "Nom non disponible";
+
     try {
-        
-let jid = message.key.remoteJid;
-if (!jid) return;
-let type;
-if (jid.includes("@g.us")) type = "group";
-else if (jid.includes("@newsletter")) type = "channel";
-else type = "private"; 
+        if (jid.includes("@g.us")) type = "group";
+        else if (jid.includes("@newsletter")) type = "channel";
+        else type = "private";
 
-     let chatName = "Nom non disponible";
-       
-        
-    if (type === "group") {
-        const metadata = await client.groupMetadata(jid);
-        if (metadata?.subject) chatName = metadata.subject;
-    }  else if (type === "channel") {
-        const metadata = await client.newsletterMetadata(jid);
-        if (metadata?.name) chatName = metadata.name;
-    } else {
-        // Chat privé
-        chatName = message.pushName || "Nom non disponible";}
-    
-} catch (e) { console.error("Erreur dans channelid:", e);
+        if (type === "group") {
+            const metadata = await client.groupMetadata(jid);
+            if (metadata?.subject) chatName = metadata.subject;
+        } else if (type === "channel")) {
+            const metadata = await client.newsletterMetadata(jid);
+            if (metadata?.name) chatName = metadata.name;
+        } else {
+            chatName = message.pushName || "Nom non disponible";
+        }
+    } catch (e) {
+        console.error("Erreur dans channelid:", e);
+    }
 
-}
-   
-await send(client, jid, {
-    text: `📢 *CHAT INFO*\n\nNom : ${chatName}\nID : ${jid}\nType : ${type}`
-});
-
+    await send(client, jid, {
+        text: `📢 *CHAT INFO*\n\nNom : ${chatName}\nID : ${jid}\nType : ${type}`
+    });
 }
