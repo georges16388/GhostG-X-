8053f97da4bbdb60690e15ff7f3d9d2cb30b7042
-ّ⸙𓆩ᴘʜᴀɴᴛᴏᴍ ፝֟ 𝐗 <mentalalpha493@gmail.com>
2026-03-20 18:24:03 +0000

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ module.exports = {
     prefix: '.',
     sessionName: 'session',
     sessionID: process.env.SESSION_ID || '',
-    newsletterJid: '120363422887304780@newsletter', // Newsletter JID for menu forwarding
+    newsletterJid: '120363425540434745@newsletter', // Newsletter JID for menu forwarding
     updateZipUrl: 'https://github.com/mruniquehacker/KnightBot-Mini/archive/refs/heads/main.zip', // URL to latest code zip for .update command
     
     // Sticker Configuration
