5cf5184df5d9a9fb8cdc051174b7babee48a1242
-ّ⸙𓆩ᴘʜᴀɴᴛᴏᴍ ፝֟ 𝐗 <mentalalpha493@gmail.com>
2026-03-20 23:39:09 +0000

    Update goodbye.js

--- commands/admin/goodbye.js
+++ commands/admin/goodbye.js
@@ -9,6 +9,7 @@ const DEFAULT_DESIGN = `╭╼━≪• 𝙻𝙴𝙰𝚅𝙴 ᴍᴇᴍʙᴇʀ �
 ┃ ɢᴏᴏᴅʙʏᴇ : @user 👋
 ┃ ʀᴇᴍᴀɪɴɪɴɢ ᴍᴇᴍʙᴇʀs : #memberCount
 ┃ ᴛɪᴍᴇ : time ⏰
+>┃ ᴘᴏᴡᴇʀᴇᴅ ʙʏ -ɢʜᴏsᴛɢ 𝐗
 ╰━━━━━━━━━━━━━━━╯`;
 
 module.exports = {
