6a82bcb17229f10506876f7af6bb4ec842129c06
-ّ⸙𓆩ᴘʜᴀɴᴛᴏᴍ ፝֟ 𝐗 <mentalalpha493@gmail.com>
2026-03-28 12:08:57 +0000

    Update antigroupmention.js

--- commands/admin/antigroupmention.js
+++ commands/admin/antigroupmention.js
@@ -26,8 +26,7 @@ const AGM_STATUS = (status, action) => {
 *┃* ⚙️ *${toStyledCaps('ᴀᴄᴛɪᴏɴ')}* : *${toStyledCaps(action)}*
 *┃* 🌐 *${toStyledCaps('sᴄᴏᴘᴇ')}* : *${toStyledCaps('ɢʀᴏᴜᴘ ᴏɴʟʏ')}*
 *┃*
-*╰━━━━━━━━━━━━━━━╯*
-> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɢʜᴏsᴛɢ-𝐗`;
+*╰━━━━━━━━━━━━━━━╯*`;
 };
 
 module.exports = {
