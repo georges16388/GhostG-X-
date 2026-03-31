69f4f53ec3d4e71683acb68a4bd6e7c4564048f4
-ّ⸙𓆩ᴘʜᴀɴᴛᴏᴍ ፝֟ 𝐗 <mentalalpha493@gmail.com>
2026-03-31 23:26:41 +0000

    Create Impressive.js

---
+++ commands/ғᴏʀɢᴇ ᴅᴇ ᴛᴇxᴛᴇ/Impressive.js
@@ -0,0 +1,46 @@
+/**
+ * Impressive Text Effect
+ */
+
+const mumaker = require('mumaker');
+const config = require('../../config');
+
+module.exports = {
+  name: 'impressive',
+  aliases: [],
+  category: '‎✎ ғᴏʀɢᴇ ᴅᴇ ᴛᴇxᴛᴇ',
+  description: 'Create impre  ssive text effect',
+  usage: '.impressive <text>',
+  
+  async execute(sock, msg, args) {
+  const prefix = config.prefix || '.'; 
+    try {
+      const text = args.join(' ');
+      const chatId = msg.key.remoteJid;
+      
+      if (!text) {
+        return await sock.sendMessage(chatId, { 
+          text:`*ғᴏᴜʀɴɪs ᴜɴ ᴛᴇxᴛᴇ*.\n *ᴇxᴀᴍᴘʟᴇ* : ${prefix}1917 GhostG-X`' 
+        }, { quoted: msg });
+      }
+      
+      const result = await mumaker.ephoto('https://en.ephoto360.com/create-3d-colorful-paint-text-effect-online-801.html', text);
+      
+      if (!result || !result.image) {
+        throw new Error('No image URL received from the API');
+      }
+      
+      await sock.sendMessage(chatId, {
+        image: { url: result.image },
+        caption: `>  *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɢʜᴏsᴛɢ-𝐗*`
+      }, { quoted: msg });
+      
+    } catch (error) {
+      console.error('Error in impressive command:', error);
+      await sock.sendMessage(msg.key.remoteJid, { 
+        text: `Error: ${error.message}` 
+      }, { quoted: msg });
+    }
+  }
+};
+
