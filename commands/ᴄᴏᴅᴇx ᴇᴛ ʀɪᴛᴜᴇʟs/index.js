7e8ff117c24373f13e6c914efda500806b0cb8a6
-ّ⸙𓆩ᴘʜᴀɴᴛᴏᴍ ፝֟ 𝐗 <mentalalpha493@gmail.com>
2026-04-01 22:39:53 +0000

    Update index.js

--- commands/ᴄᴏᴅᴇx ᴇᴛ ʀɪᴛᴜᴇʟs/index.js
+++ commands/ᴄᴏᴅᴇx ᴇᴛ ʀɪᴛᴜᴇʟs/index.js
@@ -67,7 +67,7 @@ module.exports = {
           menuText += `╭╼━≪• *${catKey}* •≫━╾╮\n`;
           const sortedCmds = cmdList.sort((a, b) => a.name.localeCompare(b.name));
           sortedCmds.forEach(cmd => {
-            menuText += `┃➻ **${toBoldSmallCaps(cmd.name)}**\n`;
+            menuText += `┃➻ ${toBoldSmallCaps(cmd.name)}\n`;
           });
           menuText += `╰━━━━━━━━━━━━━━━━━━━━━━━╯\n\n`;
         }
