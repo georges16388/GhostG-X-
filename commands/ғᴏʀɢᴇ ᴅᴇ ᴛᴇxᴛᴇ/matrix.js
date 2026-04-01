87f7fcaabc7c644d5bfd59dab751a085f1a35b7f
-ّ⸙𓆩ᴘʜᴀɴᴛᴏᴍ ፝֟ 𝐗 <mentalalpha493@gmail.com>
2026-04-01 10:37:11 +0000

    Update matrix.js

--- commands/ғᴏʀɢᴇ ᴅᴇ ᴛᴇxᴛᴇ/matrix.js
+++ commands/ғᴏʀɢᴇ ᴅᴇ ᴛᴇxᴛᴇ/matrix.js
@@ -12,7 +12,7 @@ module.exports = {
   name: 'matrix',
   aliases: [],
   category: '‎✎ ғᴏʀɢᴇ ᴅᴇ ᴛᴇxᴛᴇ',
-  description: '**ᴄʀᴇ́ᴇ ᴜɴ ᴇғғᴇᴛ ᴅᴇ ᴛᴇxᴛᴇ sᴛʏʟᴇ ᴍᴀᴛʀɪx**',
+  description: '**『 ɢʜᴏsᴛɢ-𝐗 』➪ ᴄʀᴇ́ᴇ ᴜɴ ᴇғғᴇᴛ ᴅᴇ ᴛᴇxᴛᴇ sᴛʏʟᴇ ᴍᴀᴛʀɪx**',
   usage: `${prefix}matrix <ᴛᴇxᴛᴇ>`,
 
   async execute(sock, msg, args) {
