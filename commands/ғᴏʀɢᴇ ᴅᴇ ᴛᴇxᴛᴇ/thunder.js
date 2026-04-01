bbb8f9abbfe9e101fe37c4e6208c7f2cb17acfad
-ّ⸙𓆩ᴘʜᴀɴᴛᴏᴍ ፝֟ 𝐗 <mentalalpha493@gmail.com>
2026-04-01 10:38:37 +0000

    Update thunder.js

--- commands/ғᴏʀɢᴇ ᴅᴇ ᴛᴇxᴛᴇ/thunder.js
+++ commands/ғᴏʀɢᴇ ᴅᴇ ᴛᴇxᴛᴇ/thunder.js
@@ -12,7 +12,7 @@ module.exports = {
   name: 'thunder',
   aliases: [],
   category: '‎✎ ғᴏʀɢᴇ ᴅᴇ ᴛᴇxᴛᴇ',
-  description: '**ᴄʀᴇ́ᴇ ᴜɴ ᴇғғᴇᴛ ᴅᴇ ᴛᴇxᴛᴇ sᴛʏʟᴇ ᴛʜᴜɴᴅᴇʀ**',
+  description: '**『 ɢʜᴏsᴛɢ-𝐗 』➪ ᴄʀᴇ́ᴇ ᴜɴ ᴇғғᴇᴛ ᴅᴇ ᴛᴇxᴛᴇ sᴛʏʟᴇ ᴛʜᴜɴᴅᴇʀ**',
   usage: `${prefix}thunder <ᴛᴇxᴛᴇ>`,
 
   async execute(sock, msg, args) {
