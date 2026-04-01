7e0d330137d321de7dfd216c2a2ed86aefd501b2
-ّ⸙𓆩ᴘʜᴀɴᴛᴏᴍ ፝֟ 𝐗 <mentalalpha493@gmail.com>
2026-04-01 02:13:34 +0000

    Update qr.js

--- commands/ᴄᴏᴅᴇx ᴇᴛ ʀɪᴛᴜᴇʟs/qr.js
+++ commands/ᴄᴏᴅᴇx ᴇᴛ ʀɪᴛᴜᴇʟs/qr.js
@@ -9,7 +9,7 @@ module.exports = {
   name: 'ʀᴇғʟᴇᴛ',
   aliases: ['qrcode', 'qr', 'reflet'],
   category: '☬ᴄᴏᴅᴇx ᴇᴛ ʀɪᴛᴜᴇʟs',
-  description: ' *ɢᴇ́ɴᴇ̀ʀᴇ ᴜɴ sᴄᴇᴀᴜ ǫʀ ᴀ̀ ᴘᴀʀᴛɪʀ ᴅ\'ᴜɴ ᴛᴇxᴛᴇ*',
+  description: ' **ɢᴇ́ɴᴇ̀ʀᴇ ᴜɴ sᴄᴇᴀᴜ ǫʀ ᴀ̀ ᴘᴀʀᴛɪʀ ᴅ\'ᴜɴ ᴛᴇxᴛᴇ**',
   usage: '.ʀᴇғʟᴇᴛ <text>',
   
   async execute(sock, msg, args, extra) {
