61cedd670949a5ed8cf425b575a3680657750f04
-ّ⸙𓆩ᴘʜᴀɴᴛᴏᴍ ፝֟ 𝐗 <mentalalpha493@gmail.com>
2026-04-02 13:08:52 +0000

    Update 1917.js

--- commands/ғᴏʀɢᴇ ᴅᴇ ᴛᴇxᴛᴇ/1917.js
+++ commands/ғᴏʀɢᴇ ᴅᴇ ᴛᴇxᴛᴇ/1917.js
@@ -12,7 +12,7 @@ module.exports = {
   name: '1917',
   aliases: [],
   category: '‎✎ ғᴏʀɢᴇ ᴅᴇ ᴛᴇxᴛᴇ',
-  description: '**『 ɢʜᴏsᴛɢ-𝐗 』➪ ᴄʀᴇ́ᴇ ᴜɴ ᴇғғᴇᴛ ᴅᴇ ᴛᴇxᴛᴇ sᴛʏʟᴇ 1917**',
+  description: '『 ɢʜᴏsᴛɢ-𝐗 』➪ ᴄʀᴇ́ᴇ ᴜɴ ᴇғғᴇᴛ ᴅᴇ ᴛᴇxᴛᴇ sᴛʏʟᴇ 1917',
   usage: `${prefix}1917 <ᴛᴇxᴛᴇ>`,
   
   async execute(sock, msg, args) {
@@ -34,7 +34,7 @@ module.exports = {
       
       await sock.sendMessage(chatId, {
         image: { url: result.image },
-        caption: `> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɢʜᴏsᴛɢ-𝐗*`
+        caption: `> *♰ ᴇ́ᴛᴀʙʟɪ ᴘᴀʀ ɢʜᴏsᴛɢ-𝐗 ♰*`
       }, { quoted: msg });
       
     } catch (error) {
