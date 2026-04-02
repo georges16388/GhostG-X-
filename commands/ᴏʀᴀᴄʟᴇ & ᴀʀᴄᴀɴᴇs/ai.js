af7106d8fbe565c85311c45aba2fa5b2ed2c1cd4
-ّ⸙𓆩ᴘʜᴀɴᴛᴏᴍ ፝֟ 𝐗 <mentalalpha493@gmail.com>
2026-04-02 07:40:22 +0000

    Update ai.js

--- commands/ᴏʀᴀᴄʟᴇ & ᴀʀᴄᴀɴᴇs/ai.js
+++ commands/ᴏʀᴀᴄʟᴇ & ᴀʀᴄᴀɴᴇs/ai.js
@@ -21,8 +21,8 @@ function toSmallCaps(text) {
 }
 
 module.exports = {
-  name: 'oracle',
-  aliases: ['gpt', 'chatgpt', 'ask', 'ai'],
+  name: 'ai',
+  aliases: ['gpt', 'chatgpt', 'ask'],
   category: '⍟ ᴏʀᴀᴄʟᴇ & ᴀʀᴄᴀɴᴇs',
   description: '『 ɢʜᴏsᴛɢ-𝐗 』➪ ɪɴᴠᴏǫᴜᴇ ʟᴀ sᴀɢᴇssᴇ ᴅᴇ ʟ ɪᴀ ᴘᴏᴜʀ ʀᴇᴘᴏɴᴅʀᴇ ᴀ ᴛᴇs ǫᴜᴇsᴛɪᴏɴs',
   usage: `${config.prefix || '.'}oracle <question>`,
