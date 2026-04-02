c03cbd8110b664f8c02c2080ab5a751cd387e222
-ّ⸙𓆩ᴘʜᴀɴᴛᴏᴍ ፝֟ 𝐗 <mentalalpha493@gmail.com>
2026-04-02 13:15:04 +0000

    Update sand.js

--- commands/ғᴏʀɢᴇ ᴅᴇ ᴛᴇxᴛᴇ/sand.js
+++ commands/ғᴏʀɢᴇ ᴅᴇ ᴛᴇxᴛᴇ/sand.js
@@ -12,7 +12,7 @@ module.exports = {
   name: 'sand',
   aliases: [],
   category: '‎✎ ғᴏʀɢᴇ ᴅᴇ ᴛᴇxᴛᴇ',
-  description: '**『 ɢʜᴏsᴛɢ-𝐗 』➪ ᴄʀᴇ́ᴇ ᴜɴ ᴇғғᴇᴛ ᴅᴇ ᴛᴇxᴛᴇ sᴛʏʟᴇ sᴀɴᴅ**',
+  description: '『 ɢʜᴏsᴛɢ-𝐗 』➪ ᴄʀᴇ́ᴇ ᴜɴ ᴇғғᴇᴛ ᴅᴇ ᴛᴇxᴛᴇ sᴛʏʟᴇ sᴀɴᴅ',
   usage: `${prefix}sand <ᴛᴇxᴛᴇ>`,
 
   async execute(sock, msg, args) {
@@ -34,7 +34,7 @@ module.exports = {
 
       await sock.sendMessage(chatId, {
         image: { url: result.image },
-        caption: `> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɢʜᴏsᴛɢ-𝐗*`
+        caption: `> *♰ ᴇ́ᴛᴀʙʟɪ ᴘᴀʀ ɢʜᴏsᴛɢ-𝐗 ♰*`
       }, { quoted: msg });
 
     } catch (error) {
