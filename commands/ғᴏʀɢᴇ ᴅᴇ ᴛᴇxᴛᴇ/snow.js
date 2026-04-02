77b19b47c0c6de5e4811165f0ac6665c7da8ae62
-ّ⸙𓆩ᴘʜᴀɴᴛᴏᴍ ፝֟ 𝐗 <mentalalpha493@gmail.com>
2026-04-02 13:15:35 +0000

    Update snow.js

--- commands/ғᴏʀɢᴇ ᴅᴇ ᴛᴇxᴛᴇ/snow.js
+++ commands/ғᴏʀɢᴇ ᴅᴇ ᴛᴇxᴛᴇ/snow.js
@@ -12,7 +12,7 @@ module.exports = {
   name: 'snow',
   aliases: [],
   category: '‎✎ ғᴏʀɢᴇ ᴅᴇ ᴛᴇxᴛᴇ',
-  description: '**『 ɢʜᴏsᴛɢ-𝐗 』➪ ᴄʀᴇ́ᴇ ᴜɴ ᴇғғᴇᴛ ᴅᴇ ᴛᴇxᴛᴇ sᴛʏʟᴇ sɴᴏᴡ**',
+  description: '『 ɢʜᴏsᴛɢ-𝐗 』➪ ᴄʀᴇ́ᴇ ᴜɴ ᴇғғᴇᴛ ᴅᴇ ᴛᴇxᴛᴇ sᴛʏʟᴇ sɴᴏᴡ',
   usage: `${prefix}snow <ᴛᴇxᴛᴇ>`,
 
   async execute(sock, msg, args) {
@@ -34,7 +34,7 @@ module.exports = {
 
       await sock.sendMessage(chatId, {
         image: { url: result.image },
-        caption: `> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɢʜᴏsᴛɢ-𝐗*`
+        caption: `> *♰ ᴇ́ᴛᴀʙʟɪ ᴘᴀʀ ɢʜᴏsᴛɢ-𝐗 ♰*`
       }, { quoted: msg });
 
     } catch (error) {
