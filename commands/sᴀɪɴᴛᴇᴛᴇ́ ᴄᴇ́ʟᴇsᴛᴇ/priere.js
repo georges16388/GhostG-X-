6027985b4654d1991bde72fee9e545ddc4340d08
-ّ⸙𓆩ᴘʜᴀɴᴛᴏᴍ ፝֟ 𝐗 <mentalalpha493@gmail.com>
2026-04-02 11:15:26 +0000

    Update priere.js

--- commands/sᴀɪɴᴛᴇᴛᴇ́ ᴄᴇ́ʟᴇsᴛᴇ/priere.js
+++ commands/sᴀɪɴᴛᴇᴛᴇ́ ᴄᴇ́ʟᴇsᴛᴇ/priere.js
@@ -15,7 +15,7 @@ module.exports = {
   name: 'prier',
   aliases: ['priere', 'prière','interceder', 'oraison', 'ᴘʀɪᴇʀ', 'pr'],
   category: '♰ sᴀɪɴᴛᴇᴛᴇ́ ᴄᴇ́ʟᴇsᴛᴇ',
-  description: '**『 ɢʜᴏsᴛɢ-𝐗 』➪ ɪɴᴠᴏǫᴜᴇ ᴜɴᴇ ᴘᴜɪssᴀɴᴛᴇ ᴘʀɪᴇ̀ʀᴇ ᴍʏsᴛɪǫᴜᴇ ᴅ\'ɪɴᴛᴇʀᴄᴇssɪᴏɴ**',
+  description: '『 ɢʜᴏsᴛɢ-𝐗 』➪ ɪɴᴠᴏǫᴜᴇ ᴜɴᴇ ᴘᴜɪssᴀɴᴛᴇ ᴘʀɪᴇ̀ʀᴇ ᴍʏsᴛɪǫᴜᴇ ᴅ\'ɪɴᴛᴇʀᴄᴇssɪᴏɴ',
   usage: `${prefix}prier [@user ou en réponse à un message]`,
 
   async execute(sock, msg, args, extra) {
